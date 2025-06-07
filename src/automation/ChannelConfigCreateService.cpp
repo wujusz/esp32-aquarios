2e6c420cb89ce07da56665d6a7e0da7086b63e7d
wujusz <wujusz@gmail.com>
2025-06-07 21:09:09 +0200

    Refactor channel creation service

--- src/automation/ChannelConfigCreateService.cpp
+++ src/automation/ChannelConfigCreateService.cpp
@@ -1,6 +1,7 @@
 #include "ChannelConfigCreateService.h"
 #include "Homeassistant.h"
 #include "Utilities.h"
+#include "ChannelFileUtils.h"
 
 ChannelConfigCreateService::ChannelConfigCreateService(AsyncWebServer* server,
                                                        FS* fs,
@@ -28,61 +29,10 @@ void ChannelConfigCreateService::handleCreate(AsyncWebServerRequest* request, Js
     return;
   }
 
-  File root = _fs->open(FS_CONFIG_DIRECTORY);
-  int index = 0;
-  if (root) {
-    while (true) {
-      File f = root.openNextFile();
-      if (!f) break;
-      if (!f.isDirectory()) {
-        String name = f.name();
-        if (name.endsWith("State.json")) {
-          index++;
-        }
-      }
-    }
-    root.close();
-  }
-  int analogChannel = index;
-
-  String path = String(FS_CONFIG_DIRECTORY) + "/channel" + id + "State.json";
-  File file = _fs->open(path.c_str(), "w");
-  if (!file) {
-    request->send(500);
-    return;
-  }
+  int index = getNextAnalogIndex(_fs);
 
   DynamicJsonDocument doc(DEFAULT_JSON_DOCUMENT_SIZE);
-  JsonObject rootObj = doc.to<JsonObject>();
-
-  rootObj["controlPin"] = pin;
-  rootObj["homeAssistantTopicType"] = HOMEASSISTANT_TOPIC_TYPE_SWITCH;
-  rootObj["homeAssistantIcon"] = MDI_POWER;
-  rootObj["controlOn"] = DEFAULT_CONTROL_STATE;
-  rootObj["name"] = String("Channel ") + id;
-  rootObj["enabled"] = true;
-  rootObj["brightness"] = 0;
-  rootObj["enableTimeSpan"] = false;
-  rootObj["lastStartedChangeTime"] = "";
-  rootObj["nextRunTime"] = "";
-  rootObj["randomize"] = false;
-  rootObj["IPAddress"] = "";
-  rootObj["uniqueId"] = "";
-  rootObj["enableMinimumRunTime"] = false;
-  rootObj["enableRemoteConfiguration"] = false;
-  rootObj["masterIPAddress"] = "";
-  rootObj["restChannelEndPoint"] = String("/rest/channel") + id + "State";
-  rootObj["restChannelRestartEndPoint"] = String("/rest/channel") + id + "ScheduleRestart";
-  rootObj["enableDateRange"] = false;
-  rootObj["activeOutsideDateRange"] = false;
-  rootObj["buildVersion"] = BUILD_VERSION;
-  rootObj["analogChannel"] = analogChannel;
-
-  JsonArray activeDateRange = rootObj.createNestedArray("activeDateRange");
-  activeDateRange.add(DEFAULT_START_DATE_RANGE);
-  activeDateRange.add(DEFAULT_END_DATE_RANGE);
-
-  JsonObject schedule = rootObj.createNestedObject("schedule");
+  JsonObject schedule = doc.to<JsonObject>();
   schedule["runEvery"] = 15.0f;
   schedule["offAfter"] = 3.0f;
   schedule["startTimeHour"] = 8;
@@ -93,11 +43,18 @@ void ChannelConfigCreateService::handleCreate(AsyncWebServerRequest* request, Js
   schedule["endTimeMinute"] = 0;
   schedule["isOverride"] = false;
   JsonArray weekDays = schedule.createNestedArray("weekDays");
-  for (int i = 0; i < 7; i++) {
-    weekDays.add(i);
-  }
+  for (int i = 0; i < 7; i++) weekDays.add(i);
 
-  serializeJson(doc, file);
-  file.close();
+  writeChannelJson(_fs,
+                  id,
+                  pin,
+                  0,
+                  "switch",
+                  "",
+                  "",
+                  schedule,
+                  0,
+                  index);
+
   request->send(200);
 }
